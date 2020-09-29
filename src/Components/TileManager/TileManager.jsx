9d23248a21acfacf4e01d4674973cb105b357eb1
Minkaron <aaron.tucaz@gmail.com>
2020-09-29 15:26:11 -0700

    Adding basic hook components

---
+++ src/Components/TileManager/TileManager.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+TileManager = () => {
+    const [tileList, setTileList] = useState([]);
+
+    useEffect(() => {
+
+    })
+
+    return (
+        <div>
+            <p>Tile Manager</p>
+        </div>
+    );
+}
