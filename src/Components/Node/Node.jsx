705850d36cbf3dd325ab5edd7ca2ddd83fd03968
Brian Kelly <briankelly@Brians-Mac-mini.local>
2020-09-29 16:24:42 -0700

    fix end node location

--- src/Components/Node/Node.jsx
+++ src/Components/Node/Node.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+
 import './Node.css'
+
 const Node = ({ isStart, isEnd, row, col }) => {
+    // Pass state to node so it knows if it is a start point or an end point and give the node the respective class
     const classes = isStart ? 'node-start' : isEnd ? 'node-end' : '';
     return (
         <div className={`node ${classes}`} id={`node-${row}-${col}`}></div>
     )
 }
+
 export default Node;
