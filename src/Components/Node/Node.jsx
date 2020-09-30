6ade7ca2adc98b4a47403e82d819cc6b3529032b
Brian Kelly <briankelly@Brians-Mac-mini.local>
2020-09-29 22:26:48 -0700

    add pathfinder component

--- src/Components/Node/Node.jsx
+++ src/Components/Node/Node.jsx
@@ -4,9 +4,9 @@ import './Node.css'
 
 const Node = ({ isStart, isEnd, row, col }) => {
     // Pass state to node so it knows if it is a start point or an end point and give the node the respective class
-    const classes = isStart ? 'node-start' : isEnd ? 'node-end' : '';
+    const classInit = isStart ? 'nodeStart' : isEnd ? 'nodeEnd' : '';
     return (
-        <div className={`node ${classes}`} id={`node-${row}-${col}`}></div>
+        <div className={`node ${classInit}`} id={`nodeC${col}R${row}`}></div>
     )
 }
 
